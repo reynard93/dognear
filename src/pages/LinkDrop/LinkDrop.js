9bb04396b389129e40b44b575c6fe1c8b5cc9529
Ivan <ikharchenko2008@gmail.com>
2021-10-05 14:58:49 +0300

    update LinkDrop

--- src/pages/LinkDrop/LinkDrop.js
+++ src/pages/LinkDrop/LinkDrop.js
@@ -3,58 +3,57 @@ import { ReactSVG } from 'react-svg';
 import ShareSocialLinks from '../../components/ShareSocialLinks';
 import Buy from '../../components/Buy';
 import bigCat from '../../assets/images/hero-big-cat.svg';
+import background from '../../assets/images/link-drop-background.svg';
+import ShareableLink from './ShareableLink';
+import ShareableInput from './ShareableInput';
+import ShareableCircle from './ShareableCircle';
 
 const LinkDrop = () => (
   <div className="link-drop">
-    <div className="link-drop__data">
-      <header className="link-drop__header">
-        <span className="link-drop__header-red">Share</span> a mystery{' '}
-        <span className="link-drop__header-red">NFT</span> with your friend
-      </header>
+    <div className="link-drop__data-wrapper">
+      <ReactSVG src={background} className="link-drop__background" />
 
-      <div className="link-drop__center">
-        <div>
-          <ul className="link-drop__list">
-            <li className="link-drop__item">
-              <div className="link-drop__circle"></div>
-              <div className="link-drop__text">
-                https://genc.win/?ref_id=VEDZHXYCS
-              </div>
-              <div className="link-drop__text">events@generationcrypto.org</div>
-            </li>
-            <li className="link-drop__item">
-              <div className="link-drop__circle"></div>
-              <div className="link-drop__text">
-                https://genc.win/?ref_id=ABYTMDDPE
-              </div>
-              <div className="link-drop__text">
-                Jake from Launch House on Slack
-              </div>
-            </li>
-            <li className="link-drop__item">
-              <div className="link-drop__circle"></div>
-              <div className="link-drop__text">
-                https://genc.win/?ref_id=YYCHYBETU
-              </div>
-              <div className="link-drop__text">
-                Ross Gates from NEAR on Telegram
-              </div>
-            </li>
-          </ul>
-          <div className="link-drop__save">
-            <button className="link-drop__save-btn" type="button">
-              Save
-            </button>
+      <div className="link-drop__data">
+        <header className="link-drop__header">
+          <span className="link-drop__header-red">Share</span> a mystery{' '}
+          <span className="link-drop__header-red">NFT</span> with your friend
+        </header>
+
+        <div className="link-drop__center">
+          <div>
+            <ul className="link-drop__list">
+              <li className="link-drop__item">
+                <ShareableCircle />
+                <ShareableLink link="https://genc.win/?ref_id=VEDZHXYCSsdmfld" />
+                <ShareableInput />
+              </li>
+              <li className="link-drop__item">
+                <ShareableCircle />
+                <ShareableLink link="https://genc.win/?ref_id=ABYTMDDPE" />
+                <ShareableInput />
+              </li>
+              <li className="link-drop__item">
+                <ShareableCircle />
+                <ShareableLink link="https://genc.win/?ref_id=YYCHYBETU" />
+                <ShareableInput />
+              </li>
+            </ul>
+            <div className="link-drop__save">
+              <button className="link-drop__save-btn" type="button">
+                Save
+              </button>
+            </div>
+
+            <ShareSocialLinks
+              color="purpure"
+              className="link-drop__share-links"
+            />
           </div>
-
-          <ShareSocialLinks
-            color="purpure"
-            className="link-drop__share-links"
-          />
+          <ReactSVG src={bigCat} />
         </div>
-        <ReactSVG src={bigCat} />
       </div>
     </div>
+
     <Buy />
   </div>
 );
