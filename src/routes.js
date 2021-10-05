49a1797d93a939233cf356dd163b51a257881de9
Ivan <ikharchenko2008@gmail.com>
2021-10-05 14:57:15 +0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import LinkDrop from './pages/LinkDrop/LinkDrop';
 import MyNFTS from './pages/MyNFTS';
 import NoNfts from './pages/NoNfts';
+import Reveal from './pages/Reveal';
 
 const routes = [
   {
@@ -25,6 +26,11 @@ const routes = [
     path: '/no-nfts',
     key: 'NO_NFTS',
     component: () => <NoNfts />,
+  },
+  {
+    path: '/reveal',
+    key: 'REVEAL',
+    component: () => <Reveal />,
   },
 ];
 
